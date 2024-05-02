e64d47129666228dc9d906efef6d0debe80e4ed1
username-Firda Hasanah <Firda>
2024-05-03 00:07:53 +0700

    tugas kuis wp2

---
+++ application/views/v_about.php
@@ -0,0 +1,30 @@
+<section>
+
+    <h1><?php echo $judul ?></h1>
+
+        <h4>Nama</h4>
+
+        <ul type="disc">
+            <li>Nama Depan : Firda</li>
+            <li>Nama Belakang : Hasanah</li>
+        </ul>
+        <br>
+
+        <h4>Alamat</h4>
+        <ul type="none">
+            <li> Jl.Pedurenan Mesjd 2 No.19 Jakarta Selatan</li>
+        </ul>
+
+        <h4>Tempat/Tanggal Lahir</h4>
+        <ul type="none">
+            <li>3 May 2003</li>
+            <li>Jakarta</li>
+        </ul>
+
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+            <li>Bermain Bola Basket</li>
+            <li>Berenang</li>
+        </ul>
+
+</section>
